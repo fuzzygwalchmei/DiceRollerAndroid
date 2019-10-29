f2688abfaeb9fdd19b0740f86cb301190166f6cb
Marc Falzon <fuzzygwalchmei@users.noreply.github.com>
2019-10-29 15:05:01 +1100

    removed split_dice from getItems

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -10,7 +10,10 @@ def strip_regex(regex, string):
 def roll_dice(number=1, dice=6):
     return [randint(1,dice) for n in range(number)]
 
+def split_dice(diceInput):
+    return [re.split('[d D]',dice) for dice in strip_regex(diceRegex, diceInput)]
+
 def getItems(diceInput):
-    rolls = [roll_dice(int(x),int(y)) for [x,y] in [re.split('[d D]',dice) for dice in strip_regex(diceRegex, diceInput)]]
+    rolls = [roll_dice(int(x),int(y)) for [x,y] in split_dice(diceInput)]
     mods = [int(mod.replace(" ","")) for mod in strip_regex(modRegex,diceInput)]
     return {'dice_string': diceInput, 'rolls':rolls, 'mods': mods, 'total': sum(sum(rolls, []), sum(mods))}
