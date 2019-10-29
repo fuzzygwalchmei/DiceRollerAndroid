5226ff803875d133c4d1582302edc6f880a373e1
Marc Falzon <fuzzygwalchmei@users.noreply.github.com>
2019-10-29 13:03:27 +1100

    added update for mods component
    reduced rolls and mods to list comprehensions

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -4,27 +4,18 @@ import re
 diceRegex = "[\d]d[\d]"
 modRegex = r'[+-]\s?\d\b'
 
+def roll_dice(number=1, dice=6):
+    return [randint(1,dice) for n in range(number)]
 
 def getItems(diceInput):
     myDice = re.findall(diceRegex,diceInput)
     myMods = re.findall(modRegex,diceInput)
     rolls = [roll_dice(int(x),int(y)) for [x,y] in [re.split('[d D]',dice) for dice in myDice]]
-    mods = []
+    mods = [int(mod.replace(" ","")) for mod in myMods]
+    return rolls, mods
 
-
-    for each in myMods:
-        each=each.replace(" ","")
-        mods.append(int(each))
-
-    print("Rolls: ",rolls) #, sum(rolls)
-    print("Mods: ",mods) #, sum(mods)
-    sumRolls = sum(list(map(sum, rolls)))
-    sumMods = sum(mods)
-    print(sumRolls + sumMods)
-
-
-
-
-def roll_dice(number=1, dice=6):
-    return [randint(1,dice) for n in range(number)]
-
+print("Rolls: ",rolls) #, sum(rolls)
+print("Mods: ",mods) #, sum(mods)
+sumRolls = sum(list(map(sum, rolls)))
+sumMods = sum(mods)
+print(sumRolls + sumMods)
