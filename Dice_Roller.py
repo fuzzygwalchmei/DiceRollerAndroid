44e149b0204f29c2240421edd6ca625a3c5d9c6b
Marc Falzon <fuzzygwalchmei@users.noreply.github.com>
2019-10-29 14:21:53 +1100

    refactored for strip_regex and the returned dictionary

--- Dice_Roller.py
+++ Dice_Roller.py
@@ -1,21 +1,16 @@
 from random import randint
 import re
 
-diceRegex = "[\d]d[\d]"
-modRegex = r'[+-]\s?\d\b'
+diceRegex = r'[\d]+d[\d]+'
+modRegex = r'[+-]\s?\d+\b'
+
+def strip_regex(regex, string):
+    return re.findall(reg, string)
 
 def roll_dice(number=1, dice=6):
     return [randint(1,dice) for n in range(number)]
 
 def getItems(diceInput):
-    myDice = re.findall(diceRegex,diceInput)
-    myMods = re.findall(modRegex,diceInput)
-    rolls = [roll_dice(int(x),int(y)) for [x,y] in [re.split('[d D]',dice) for dice in myDice]]
-    mods = [int(mod.replace(" ","")) for mod in myMods]
-    return rolls, mods
-
-print("Rolls: ",rolls) #, sum(rolls)
-print("Mods: ",mods) #, sum(mods)
-sumRolls = sum(list(map(sum, rolls)))
-sumMods = sum(mods)
-print(sumRolls + sumMods)
+    rolls = [roll_dice(int(x),int(y)) for [x,y] in [re.split('[d D]',dice) for dice in strip_regex(diceRegex, diceInput)]]
+    mods = [int(mod.replace(" ","")) for mod in strip_regex(modRegex,diceInput)]
+    return {'dice_string': diceInput, 'rolls':rolls, 'mods': mods, 'total': sum(sum(rolls, []), sum(mods))}
